12bf465f8a3aec66de67038be01fd2138c4a8420
M. Pennella <michael.pennella@gmail.com>
2018-11-15 02:36:49 -0800

    Added new files and basic DB schema

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ("Thank God It's Fried Egg Burger",TRUE);
+
+INSERT INTO burgers (burger_name,devoured) VALUES ("Olive and Let Die Burger",FALSE);
+
+INSERT INTO burgers (burger_name,devoured) VALUES ("Poblano Picasso Burger",TRUE);
+
+INSERT INTO burgers (burger_name,devoured) VALUES ("Good Night and Good Leek Burger",TRUE);
