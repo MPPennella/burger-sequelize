bd58c951bb0243a07ec8b5d46fbce49982498f99
M. Pennella <michael.pennella@gmail.com>
2018-11-15 15:18:35 -0800

    Created basic controller structure

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,12 @@
+// Import required modules
+const express = require("express")
+const burger = require("../models/burger")
+
+// Set up Express router
+const router = express.Router()
+
+// Route Definitions
+
+
+// Export router
+module.exports = router
