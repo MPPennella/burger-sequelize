cb10ef15bc26ad7d38eb2e574aaeaf105d1ab416
M. Pennella <michael.pennella@gmail.com>
2018-11-15 15:35:48 -0800

    Added server logic

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+// Import required modules
 const express = require("express")
+const exphbs = require("express-handlebars")
+const routes = require("./controllers/burgers_controller")
+
+// Define listening port
+const PORT = process.env.PORT || 8080
+
+// Initialize and configure app
+const app = express()
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"))
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Configure to use handlebars for rendering
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+// Add route controller
+app.use(routes)
+
+// Start server
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
