b3c305648fc37b8c9666905ae296b0c30fde742d
M. Pennella <michael.pennella@gmail.com>
2018-11-15 14:41:06 -0800

    Added functionality to orm commands

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,38 @@
 const connection = require("./connection.js")
 
 const orm = {
-    selectAll: function () {
+    selectAll: function (table, cb) {
+        let query = "SELECT * FROM ??;"
 
+        connection.query(query, [table], function(err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result)
+        })
     },
 
-    insertOne: function () {
+    insertOne: function (table, cols, values, cb) {
+        let query = "INSERT INTO ?? (??) VALUES (?)"
 
+        connection.query(query, [table, cols, values], function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
     },
 
-    updateOne: function () {
+    updateOne: function (table, column, value, idCol, idVal, cb) {
+        let query = "UPDATE ?? SET ??=? WHERE ??=?"
 
+        connection.query(query, [table, column, value, idCol, idVal], function(err, result) {
+            if (err) {
+                 throw err;
+            }
+      
+            cb(result);
+        });
     }
 }
 
