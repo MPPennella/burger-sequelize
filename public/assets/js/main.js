ed75ea530e13cd000698166f1e18f27fc052233e
M. Pennella <michael.pennella@gmail.com>
2018-11-17 02:13:50 -0800

    Added POST method to submit form data

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,12 +3,24 @@ $("document").ready(function() {
     // Listener for new burger form submission
     $(".create-form").on("submit", function (event) {
         event.preventDefault();
+
+        // Gather data from form
+        let burgerData = {
+            name: $("#newBurgerName").val().trim(),
+            devoured: $("[name=eaten]:checked").val()
+        }
+        console.log(burgerData)
+
+        // Submit POST request with data
+        $.post("api/burgers", burgerData)
+        .then( function() {
+            location.reload()
+        })
     })
 
     $(".devourBtn").on("click", function () {
         // Get id from button data
         let id = $(this).data("id")
-        console.log(id)
 
         // Send PUT request to 'devour' selected id
         $.ajax(`api/burgers/devour/${id}`, {
