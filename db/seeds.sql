f7fe99dd3724b1ad7ed04abd001ad70e3568016b
M. Pennella <michael.pennella@gmail.com>
2018-11-17 03:09:38 -0800

    Added more seeds to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,8 +2,14 @@ INSERT INTO burgers (burger_name,devoured) VALUES ("Thank God It's Fried Egg Bur
 
 INSERT INTO burgers (burger_name,devoured) VALUES ("Olive and Let Die Burger",FALSE);
 
+INSERT INTO burgers (burger_name,devoured) VALUES ("License to Krill Seafood Burger",FALSE);
+
 INSERT INTO burgers (burger_name,devoured) VALUES ("Poblano Picasso Burger",TRUE);
 
 INSERT INTO burgers (burger_name,devoured) VALUES ("Good Night and Good Leek Burger",TRUE);
 
 INSERT INTO burgers (burger_name,devoured) VALUES ("Mushroom with a View Burger",FALSE);
+
+INSERT INTO burgers (burger_name,devoured) VALUES ("No-one Expects the Spinach Inquisition Burger",FALSE);
+
+INSERT INTO burgers (burger_name,devoured) VALUES ("Cheesed to Meet You Burger",FALSE);
