70fa4f48437c0728775654ddd39d48893f134e60
M. Pennella <michael.pennella@gmail.com>
2018-11-17 01:32:20 -0800

    Added static javascript file for client webpage

---
+++ public/assets/js/main.js
@@ -0,0 +1,11 @@
+$("document").ready(function() {
+
+    // Listener for new burger form submission
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+    })
+
+    $(".devourBtn").on("click", function () {
+        
+    })
+})
