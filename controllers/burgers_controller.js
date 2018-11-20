e215e7e9508d2c3adce4080a755f5bcedb427867
M. Pennella <michael.pennella@gmail.com>
2018-11-19 21:13:07 -0800

    Changed behavior of PUT route to return 'Bad Request' status if burger was already devoured, not just if id wasn't found

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -21,9 +21,6 @@ router.get("/", (req, res) => {
 // GET API route for getting a list of all burgers
 router.get("/api/burgers", (req, res) => {
     burger.getAll(data => {
-        // var burgerListObj = {
-        //      burgers: data
-        // }
         console.log(data)
         res.json(data)
     })
@@ -50,10 +47,9 @@ router.put("/api/burgers/devour/:id", (req, res) => {
     let id = req.params.id
     burger.devour(id, result => {
         console.log(result)
-        // If table had affected rows, id was found, so return 200 "OK" status
-        // Does not matter if status was changed, only that id was valid
-        if (result.affectedRows) res.status(200).end()
-        // Else table did not contain id, so return 400 "Bad Request" status
+        // If table had changed rows, id was found and status was changed, so return 200 "OK" status
+        if (result.changedRows) res.status(200).end()
+        // Else table either did not contain id or was already set to devoured, so return 400 "Bad Request" status
         else res.status(400).end()
     })
 })
