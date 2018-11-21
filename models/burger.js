c40badb7f597ec3c1a8375f637798fb70a182c97
M. Pennella <michael.pennella@gmail.com>
2018-11-20 18:28:13 -0800

    Changed model structure to use sequelize library

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,11 @@
-const orm = require("../config/orm")
-
-// Reference for burger table name
-const burgerTable = "burgers"
-
-const burger = {
-    // Create uneaten burger
-    create(name, cb) {
-        orm.insertOne(burgerTable, ["burger_name","devoured"], [name, false], res => cb(res) )
-    },
-
-    // Devour a burger
-    devour(id, cb) {
-        orm.updateOne(burgerTable, "devoured", true, "id", id, res => cb(res) )
-    },
-
-    // Get info for all burgers
-    getAll(cb) {
-        orm.selectAll(burgerTable, res => cb(res) )
-    }
+module.exports = function(sequelize, DataTypes) {
+    const Burger = sequleize.define("Burger", {
+        burger_name: {
+            type: DataTypes.STRING,
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN
+        }
+    })
+    return Burger
 }
-
-module.exports = burger
