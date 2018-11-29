d5c7408be31204e921463bcf7dd67559a896edec
M. Pennella <michael.pennella@gmail.com>
2018-11-29 03:44:56 -0800

    Fixed PUSH API route to work with sequelize

--- routes/burgers_controller.js
+++ routes/burgers_controller.js
@@ -1,5 +1,6 @@
 // Import required modules
 const db = require("../models")
+const Op = require("sequelize").Op
 
 // Route Definitions
 module.exports = function(app) {
@@ -49,11 +50,17 @@ module.exports = function(app) {
     // PUT API route for setting the status of a burger to 'devoured'
     app.put("/api/burgers/devour/:id", (req, res) => {
         let id = req.params.id
-        db.Burger.devour(id, result => {
+        db.Burger.update({
+            devoured: true
+        },{
+            where: {
+                id: id
+            }
+        }).then( result => {
             console.log(result)
-            // If table had changed rows, id was found and status was changed, so return 200 "OK" status
-            if (result.changedRows) res.status(200).end()
-            // Else table either did not contain id or was already set to devoured, so return 400 "Bad Request" status
+            // If table had affected rows, id was found, so return 200 "OK" status
+            if (result[0]) res.status(200).end()
+            // Else table did not contain id, so return 400 "Bad Request" status
             else res.status(400).end()
         })
     })
